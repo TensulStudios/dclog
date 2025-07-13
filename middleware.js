d9f0ae0574aae3dfabcdc918613e89ebbdfd9348
TensulStudios <tunfulstudios@gmail.com>
2025-07-13 13:36:40 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -9,20 +9,5 @@ export async function middleware(req){
   }
   const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
   const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    embeds:[{
-      title:"Triggered view-logger",
-      description:(source ? "Source user-agent: "+ua : "It was loaded by an user (or an user on Discord)."),
-      footer:{
-        text:"Requested page: "+page.slice(0,500),
-      },
-    }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
-  if(source){
-    // Return the image.
-    return NextResponse.rewrite(new URL("/mini.png",req.url))
-  }else{
-    // Make a message for whoever takes the risk to directly click.
-    return NextResponse.rewrite(new URL("/page.html",req.url));
-  }
+  return NextResponse.rewrite(new URL("/mini.png",req.url))
 }
